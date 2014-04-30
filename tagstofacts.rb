8459e080f55dc4ef9926222c679eb86187e5efe0
tokynet <toky.cruz@gmail.com>
2014-04-30 13:05:57 -0400

    added factstotags.rb script 1 of 3

--- tagstofacts.rb
+++ tagstofacts.rb
@@ -1,14 +1,11 @@
-# tag_name.rb
+# tagstofacts.rb
 
-#We make an ec2 call and put the tags in a dot file in tmp
-set code do /usr/bin/aws --output text ec2 describe-instances --instance-ids #{instanceid} | grep "TAGS" | awk '{ print $2 " " $3 " " $4}' > /tmp/.tags
 #Create the fact from the NAME (EC2) TAG
 Facter.add("tag_name") do
    setcode do
           region = Facter.value('region')
           instanceid = Facter.value('instanceid')
-          %x{/usr/bin/aws --region #{region} --output text ec2 describe-instances --instance-ids #{instanceid} | egrep -iw "Name" | tr [:upper:] [:lower
-:] | /bin/awk '{ print $3 }'}.chomp
+          %x{/usr/bin/aws --region #{region} --output text ec2 describe-instances --instance-ids #{instanceid} | egrep -iw "Name" | tr [:upper:] [:lower:] | /bin/awk '{ print $3 }'}.chomp
    end
 end
 
@@ -17,8 +14,7 @@ Facter.add("tag_sdlc") do
    setcode do
           region = Facter.value('region')
           instanceid = Facter.value('instanceid')
-          %x{/usr/bin/aws --region #{region} --output text ec2 describe-instances --instance-ids #{instanceid} | egrep -iw "SDLC" | tr [:upper:] [:lower
-:] | /bin/awk '{ print $3 }'}.chomp
+          %x{/usr/bin/aws --region #{region} --output text ec2 describe-instances --instance-ids #{instanceid} | egrep -iw "SDLC" | tr [:upper:] [:lower:] | /bin/awk '{ print $3 }'}.chomp
    end
 end
 
@@ -27,8 +23,7 @@ Facter.add("tag_owner") do
    setcode do
           region = Facter.value('region')
           instanceid = Facter.value('instanceid')
-          %x{/usr/bin/aws --region #{region} --output text ec2 describe-instances --instance-ids #{instanceid} | egrep -iw "OWNER" | tr [:upper:] [:lowe
-r:] | /bin/awk '{ print $3 " " $4 }'}.chomp
+          %x{/usr/bin/aws --region #{region} --output text ec2 describe-instances --instance-ids #{instanceid} | egrep -iw "OWNER" | tr [:upper:] [:lower:] | /bin/awk '{ print $3 " " $4 }'}.chomp
    end
 end
 
@@ -37,8 +32,7 @@ Facter.add("tag_ags") do
    setcode do
           region = Facter.value('region')
           instanceid = Facter.value('instanceid')
-          %x{/usr/bin/aws --region #{region} --output text ec2 describe-instances --instance-ids #{instanceid} | egrep -iw "AGS" | tr [:upper:] [:lower:
-] | /bin/awk '{ print $3 }'}.chomp
+          %x{/usr/bin/aws --region #{region} --output text ec2 describe-instances --instance-ids #{instanceid} | egrep -iw "AGS" | tr [:upper:] [:lower:] | /bin/awk '{ print $3 }'}.chomp
    end
 end
 
@@ -47,8 +41,7 @@ Facter.add("tag_purpose") do
    setcode do
           region = Facter.value('region')
           instanceid = Facter.value('instanceid')
-          %x{/usr/bin/aws --region #{region} --output text ec2 describe-instances --instance-ids #{instanceid} | egrep -iw "PURPOSE" | tr [:upper:] [:lo
-wer:] | /bin/awk '{ print $3 }'}.chomp
+          %x{/usr/bin/aws --region #{region} --output text ec2 describe-instances --instance-ids #{instanceid} | egrep -iw "PURPOSE" | tr [:upper:] [:lower:] | /bin/awk '{ print $3 }'}.chomp
    end
 end
 
@@ -57,7 +50,6 @@ Facter.add("tag_cost-center") do
    setcode do
           region = Facter.value('region')
           instanceid = Facter.value('instanceid')
-          %x{/usr/bin/aws --region #{region} --output text ec2 describe-instances --instance-ids #{instanceid} | egrep -iw "Cost Center" | tr [:upper:] 
-[:lower:] | /bin/awk '{ print $4 }'}.chomp
+          %x{/usr/bin/aws --region #{region} --output text ec2 describe-instances --instance-ids #{instanceid} | egrep -iw "Cost Center" | tr [:upper:] [:lower:] | /bin/awk '{ print $4 }'}.chomp
    end
 end
